84cf6f4e5b5456203fc6a5f72cbaf7e1095f7842
Pradyumna <praddonekal@gmail.com>
2021-06-20 12:58:06 +0530

    restructured the code

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports =
+{
+    //"URI": "mongodb://localhost/contact_list"
+    "URI":"mongodb+srv://prad:TrPjXOy7TNKuj2B0@contacts.k4ds3.mongodb.net/contact_list?retryWrites=true&w=majority"
+}
